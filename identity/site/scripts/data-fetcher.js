77f6f0fa684429a1a752af64297247d699c96642
Claudius Tiberiu Iacob <claudius.iacob@gmail.com>
2024-01-06 18:28:32 +0200

    Update data-fetcher.js

--- identity/site/scripts/data-fetcher.js
+++ identity/site/scripts/data-fetcher.js
@@ -2,3 +2,82 @@
 Uses the Github API to fetch data about general project information,
 current release availability, etc. 
 */
+
+class GitHubAPI {
+  constructor(api) {
+      this.api = api;
+  }
+
+  getReleasesList() {
+      return fetch(this.api)
+          .then(response => {
+              let result = {
+                  'responseRaw': null,
+                  'responseParsed': null,
+                  'httpStatus': response.status,
+                  'errors': [],
+                  'hasError': !response.ok
+              };
+              if (!response.ok) {
+                  result.errors.push("HTTP error " + response.status);
+              }
+              return response.text().then(text => {
+                  result.responseRaw = text;
+                  try {
+                      result.responseParsed = JSON.parse(text);
+                  } catch (error) {
+                      result.errors.push("JSON parsing error: " + error.message);
+                      result.hasError = true;
+                  }
+                  return result;
+              });
+          })
+          .catch(error => {
+              return {
+                  'responseRaw': null,
+                  'responseParsed': null,
+                  'httpStatus': -1,
+                  'errors': ["Fetch error: " + error.message],
+                  'hasError': true
+              };
+          });
+  }
+
+  getReleaseAssets(assetsUrl) {
+      return fetch(assetsUrl)
+          .then(response => {
+              let result = {
+                  'responseRaw': null,
+                  'responseParsed': null,
+                  'httpStatus': response.status,
+                  'errors': [],
+                  'hasError': !response.ok
+              };
+              if (!response.ok) {
+                  result.errors.push("HTTP error " + response.status);
+              }
+              return response.text().then(text => {
+                  result.responseRaw = text;
+                  try {
+                      result.responseParsed = JSON.parse(text);
+                  } catch (error) {
+                      result.errors.push("JSON parsing error: " + error.message);
+                      result.hasError = true;
+                  }
+                  return result;
+              });
+          })
+          .catch(error => {
+              return {
+                  'responseRaw': null,
+                  'responseParsed': null,
+                  'httpStatus': -1,
+                  'errors': ["Fetch error: " + error.message],
+                  'hasError': true
+              };
+          });
+  }
+}
+
+
+
